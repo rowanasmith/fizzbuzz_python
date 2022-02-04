985344a2f50a352ce11c3fa115de1eaf2dd43fa5
rowanasmith <45500732+rowanasmith@users.noreply.github.com>
2022-02-04 05:51:02 +0000

    operation successful

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 #Write your code below this row 👇
 
+for number in range(1, 101):
+    if number % 3 == 0:
+        if number % 5 == 0:
+            number = "FizzBuzz"
+        else:
+            number = "Fizz"
+    elif number % 5 == 0:
+        number = "Buzz"
+    print(number)
